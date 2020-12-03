b4ec7cb2cce35ea6b97284a4b588033027d203b0
taeksoolee <leets1490@gmail.com>
2020-12-03 19:28:42 +0900

    20201203 공부내용

--- blog/js/code-converter.js
+++ blog/js/code-converter.js
@@ -1,26 +1,28 @@
 //syntax에 맞는 코드 형식 변환
-String.prototype.convertHTML = function() {
-  return this
-  // 코드 안에 samp 태그를 사용하면 html 태그 사용가능 하도록 설정 (꺽쇠 무시)
-  .replaceAll(/<code>.*<\/code>/gs, function (codeText) {
-    const startcode = "codecodecode";
-    const endcode = "/code/code/code";
+String.prototype.convertHTML = function () {
+  return (
+    this
+      // 코드 안에 samp 태그를 사용하면 html 태그 사용가능 하도록 설정 (꺽쇠 무시)
+      .replaceAll(/<code>.*<\/code>/gs, function (codeText) {
+        const startcode = "codecodecode";
+        const endcode = "/code/code/code";
 
-    return codeText
-      .replaceAll("<code>", startcode)
-      .replaceAll("</code>", endcode)
-      .replaceAll(/<samp>.*<\/samp>/gs, function (sampText) {
-        return sampText
-          .replaceAll("<samp>", "")
-          .replaceAll("</samp>", "")
-          .replaceAll(/[<>]/g, function (tag) {
-            return `<span>${tag}</span>`;
-          });
+        return codeText
+          .replaceAll("<code>", startcode)
+          .replaceAll("</code>", endcode)
+          .replaceAll(/<samp>.*<\/samp>/gs, function (sampText) {
+            return sampText
+              .replaceAll("<samp>", "")
+              .replaceAll("</samp>", "")
+              .replaceAll(/[<>]/g, function (tag) {
+                return `<span>${tag}</span>`;
+              });
+          })
+          .replaceAll(startcode, "<code>")
+          .replaceAll(endcode, "</code>");
       })
-      .replaceAll(startcode, "<code>")
-      .replaceAll(endcode, "</code>");
-  });
-}
+  );
+};
 
 function covertCodeForHTML(text, language) {
   switch (language) {
@@ -38,33 +40,31 @@ function covertCodeForHTML(text, language) {
 }
 
 function convertJavascript(text) {
-  return (
-    text
-      .convertHTML()
-      .replaceAll(/(function)/g, (t) => `<span class="c-blue">${t}</span>`)
-      .replaceAll(/(if)|(else)/g, (t) => `<span class="c-green">${t}</span>`)
-      .replaceAll(
-        /(switch)|(case)|(continue)|(break)|(default)|(return)|(try)|(catch)/g,
-        (t) => `<span class="switch">${t}</span>`
-      )
-      .replaceAll(/(var)/g, (t) => `<span class="c-yellow">${t}</span>`)
-      .replaceAll(
-        /(const)|(let)/g,
-        (t) => `<span class="c-lightgreen">${t}</span>`
-      )
-      .replaceAll(
-        /(class )|(interface)|(impliments)|(extends)/g,
-        (t) => `<span class=".c-lightpurple">${t}</span>`
-      )
-      .replaceAll(
-        /(require)|(import)|(export)|(module\.export)|(export default)|(from)/g,
-        (t) => `<span class="c-lightpink">${t}</span>`
-      )
-      .replaceAll(
-        /[\+-/%=?:{}()[]]/g,
-        (t) => '<span class="c-skyblue">' + t + "</span>"
-      )
-  );
+  return text
+    .convertHTML()
+    .replaceAll(/(function)/g, (t) => `<span class="c-blue">${t}</span>`)
+    .replaceAll(/(if)|(else)/g, (t) => `<span class="c-green">${t}</span>`)
+    .replaceAll(
+      /(switch)|(case)|(continue)|(break)|(default)|(return)|(try)|(catch)/g,
+      (t) => `<span class="switch">${t}</span>`
+    )
+    .replaceAll(/(var)/g, (t) => `<span class="c-yellow">${t}</span>`)
+    .replaceAll(
+      /(const)|(let)/g,
+      (t) => `<span class="c-lightgreen">${t}</span>`
+    )
+    .replaceAll(
+      /(class )|(interface)|(impliments)|(extends)/g,
+      (t) => `<span class=".c-lightpurple">${t}</span>`
+    )
+    .replaceAll(
+      /(require)|(import)|(export)|(module\.exports)|(export default)|(from)/g,
+      (t) => `<span class="c-lightpink">${t}</span>`
+    )
+    .replaceAll(
+      /[\+-/%=?:{}()[]]/g,
+      (t) => '<span class="c-skyblue">' + t + "</span>"
+    );
 }
 
 function convertPython(text) {
