3e9d8d674d6bcb6e6ba20283a36a8c25604bd207
taeksoolee <leets1490@gmail.com>
2024-04-10 00:07:58 +0900

    Feat: pokemon

--- pokemon/script.js
+++ pokemon/script.js
@@ -13,6 +13,19 @@ function recursive(obj, prefix='') {
 };
 
 angular.module('app', ['ngRoute'])
+  .run(function($rootScope, $http, $window) {
+    $http({
+      url: './poke.json'
+    }).then(
+      function(res) {
+        $rootScope.pokenames = res.data;
+      }
+    );
+
+    $rootScope.back = function () {
+      $window.history.back();
+    };
+  })
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
@@ -23,22 +36,37 @@ angular.module('app', ['ngRoute'])
         <div ng-if="loading">
           Loading...
         </div>
-        <div ng-if="!loading">
-          <h1>Pokemons</h1>
+        <div ng-if="!loading" class="m-4">
+          <h1 class="text-4xl mb-4">Pokemons</h1>
           <div>
-            <p> Total : {{ state.total }} </p>
+            <p class="text-gray-500"> Total : {{ state.total }} </p>
           </div>
-          <nav>
-            <span ng-repeat="page in pager.pages">
-              <span ng-if="page === pager.cur">{{ page }}</span>
-              <a ng-if="page !== pager.cur" ng-href="#!/?cur={{ page }}&limit={{ pager.limit }}">{{ page }}</a>
-              &nbsp;&nbsp;
+          <nav class="mb-4">
+            <span class="inline-block w-10 h-10 mx-2 content-middle">
+              <span ng-if="pager.first === pager.cur" class="flex justify-center items-center  bg-blue-200 text-blue-500 w-full h-full">{{ pager.first }}</span>
+              <a ng-if="pager.first !== pager.cur" ng-href="#!/?cur={{ pager.first }}&limit={{ pager.limit }}" class="flex justify-center items-center w-full h-full bg-gray-200 text-gray-400 hover:text-blue-500">
+                {{ pager.first }}
+              </a>
+            </span>
+            <span ng-repeat="page in pager.pages" class="inline-block w-10 h-10 mx-2 content-middle">
+              <span ng-if="page === pager.cur" class="flex justify-center items-center  bg-blue-200 text-blue-500 w-full h-full">{{ page }}</span>
+              <a ng-if="page !== pager.cur" ng-href="#!/?cur={{ page }}&limit={{ pager.limit }}" class="flex justify-center items-center w-full h-full bg-gray-200 text-gray-400 hover:text-blue-500">
+                {{ page }}
+              </a>
+            </span>
+            <span class="inline-block w-10 h-10 mx-2 content-middle">
+              <span ng-if="pager.last === pager.cur" class="flex justify-center items-center  bg-blue-200 text-blue-500 w-full h-full">{{ pager.last }}</span>
+              <a ng-if="pager.last !== pager.cur" ng-href="#!/?cur={{ pager.last }}&limit={{ pager.limit }}" class="flex justify-center items-center w-full h-full bg-gray-200 text-gray-400 hover:text-blue-500">
+                {{ pager.last }}
+              </a>
             </span>
           </nav>
           <ul>
-            <li ng-repeat="pokemon in state.pokemons">
-              <a ng-href="#!/detail?data={{ pokemon.url | encodeUrl }}">
-                {{ pokemon.name }}
+            <li ng-repeat="pokemon in state.pokemons" class="mb-1">
+              <a ng-href="#!/detail?data={{ pokemon.url | encodeUrl }}" class="group inline-block w-full hover:text-blue-500 hover:border-b hover:border-blue-400">
+                <span class="inline-block w-14">[{{ pokemon.number | number }}]</span>
+                <span class="inline-block w-24 font-bold">{{ pokemon.number | pokename }} </span>
+                <span class="text-gray-400 group-hover:text-inherit">{{ pokemon.name }}</span>
               </a>
             </li>
           </ul>
@@ -51,20 +79,35 @@ angular.module('app', ['ngRoute'])
         <div ng-if="loading">
           Loading...
         </div>
-        <div ng-if="!loading">
-          <h1>{{ state.pokemon.name }}</h1>
-          <span>order: {{ state.pokemon.order }}</span>
+        <div ng-if="!loading" class="m-4">
+          <button ng-click="back()" class="text-gray-400 hover:text-blue-500">
+            <i class="fa-solid fa-arrow-left"></i>
+          </button>
+          <div class="border w-fit">
+            <img ng-src="{{ state.pokemon.img }}" alt="{{ state.pokemon.name }}" class="w-80 h-80"/>
+            <div class="p-4">
+              <div class="text-2xl">{{ state.pokemon.name }}</div>
+            </div>
+          </div>
+          <!--
           <div ng-repeat="sprite in state.pokemon.sprites">
             <img ng-src="{{ sprite[1] }}" ng-alt="{{ sprite[0] }}" />
             <hr />
           </div>
+          -->
         </div>
         `,
         controller: 'DetailCtrl',
       })
   })
+  .filter('number', function() {
+    return (input) => `${input}`.padStart(4, '0');
+  })
   .filter('encodeUrl', function() {
     return (input) => encodeURIComponent(input);
+  })
+  .filter('pokename', function($rootScope) {
+    return (input) => $rootScope.pokenames[input]?.[0] ?? '';
   })
   .service('api', function($http, $q) {
     this.call = function(url) {
@@ -100,7 +143,10 @@ angular.module('app', ['ngRoute'])
         api.call(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${res.data.count}`)
         .then(function(res) {
           $scope.state = {
-            pokemons: res.data.results.slice(start, end),
+            pokemons: res.data.results.map((e, i) => ({
+              ...e,
+              number: i+1,
+            })).slice(start, end),
             total: res.data.count,
           }
 
@@ -126,10 +172,27 @@ angular.module('app', ['ngRoute'])
             i++;
           }
 
+          const first = allPages.shift();
+          const last = allPages.pop();
+
+          if (pages[0] === first) {
+            pages.shift();
+            if (pages[pages.length-1]+1 <= last) {
+              pages.push(pages[pages.length-1]+1);
+            }
+          } else if (pages[pages.length-1] === last) {
+            pages.pop();
+            if (pages[0]-1 >= 0) {
+              pages.unshift(pages[0]-1);
+            }
+          }
+
           $scope.pager = {
             cur,
             limit,
             pages,
+            first,
+            last,
           }
           $scope.loading = false;
         })
@@ -150,6 +213,7 @@ angular.module('app', ['ngRoute'])
             name: res.data.name,
             order: res.data.order,
             sprites: recursive(res.data.sprites),
+            img: res.data.sprites.other.home.front_default
           }
         };
 
